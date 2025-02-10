a28e3dce1e79b7da22458d404f1ca9261de40733
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-02-10 11:36:48 +0530

    product add api

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
 
 const app = express()
 
-
+app.use('express.json');
 
 app.get('/', (req, res) => {
   res.send('Hello Worhhjmld!');
+});
+
+app.post('/api/products', async(req, res) => {
+  try {
+    const product= await Product.create(req.body);
+    res.status(200),json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 mongoose.connect("mongodb+srv://admin:admin@cruddb.bbhdm.mongodb.net/Node-Crud?retryWrites=true&w=majority&appName=cruddb")
