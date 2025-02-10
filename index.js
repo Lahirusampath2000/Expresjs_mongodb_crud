b9b1361b74218a63de8a7418ba6d1de41edc9531
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-02-10 11:06:18 +0530

    connect database

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+
 
 app.get('/', (req, res) => {
   res.send('Hello Worhhjmld!');
 });
+
+mongoose.connect("mongodb+srv://admin:admin@cruddb.bbhdm.mongodb.net/Node-Crud?retryWrites=true&w=majority&appName=cruddb")
+ .then(() => {
+   console.log('Connected to MongoDB');
+   app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
+ })
+ .catch((err) => {
+   console.log('Failed to connect to MongoDB', err);
+ });
